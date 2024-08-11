f1017aac28d0e0c7eee09885bfd9a4ca222316b9
xU <142771851+xUankip@users.noreply.github.com>
2024-08-11 14:37:32 +0700

    them-moi-usercontroller

--- resources/views/questions/question-list.blade.php
+++ resources/views/questions/question-list.blade.php
@@ -1,4 +1,91 @@
-<!doctype html>
+{{--<!doctype html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="UTF-8">--}}
+{{--    <meta name="viewport"--}}
+{{--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">--}}
+{{--    <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
+{{--    <title>Document</title>--}}
+{{--    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">--}}
+{{--    <!-- Font Awesome -->--}}
+{{--    <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">--}}
+{{--    <!-- Theme style -->--}}
+{{--    <link rel= "stylesheet" href="/dist/css/adminlte.min.css">--}}
+{{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">--}}
+{{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />--}}
+{{--    <style>--}}
+{{--        .card-header h4 {--}}
+{{--            display: inline-block;--}}
+{{--            overflow: hidden;--}}
+{{--            white-space: nowrap;--}}
+{{--            text-overflow: ellipsis;--}}
+{{--            max-width: 100%;--}}
+{{--            cursor: pointer;--}}
+{{--            transition: max-height 0.5s ease;--}}
+{{--        }--}}
+{{--        .card-header h4.expanded {--}}
+{{--            white-space: normal;--}}
+{{--            text-overflow: clip;--}}
+{{--        }--}}
+{{--    </style>--}}
+{{--</head>--}}
+{{--<form action="{{route('saveAll')}}" method="post">--}}
+{{--    @csrf--}}
+{{--    <div class="container-fluid">--}}
+{{--        <div class="row">--}}
+{{--            <input name="topic" value="{{$topic}}">--}}
+{{--            <input name="game" value="{{$game}}">--}}
+{{--            @foreach($decoded->quiz as $quiz)--}}
+{{--                <div class="col-12">--}}
+{{--                    <div class="card collapsed-card">--}}
+{{--                        <div class="card-header">--}}
+{{--                            <h3 class="card-title">Title</h3>--}}
+{{--                            <div class="card-tools">--}}
+{{--                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">--}}
+{{--                                    <i class="fas fa-plus"></i>--}}
+{{--                                </button>--}}
+{{--                                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">--}}
+{{--                                    <i class="fas fa-times"></i>--}}
+{{--                                </button>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="card-body" style="display: none;">--}}
+{{--                            Nội dung câu hỏi--}}
+{{--                            <input name="questions" type="text" class="form-control" id="exampleInputEmail1" value="{{$quiz->question}}">--}}
+{{--                        </div>--}}
+{{--                            @foreach($quiz->answers as $answers)--}}
+{{--                                <div class="card-footer" style="display: none;">--}}
+{{--                                    <div class="form-check">--}}
+{{--                                        <input class="form-check-input" type="radio" name="radio1">--}}
+{{--                                        <input type="hidden" name="answer_content" value="{{json_encode($answers)}}">--}}
+{{--                                        <input type="hidden" name="correct_answer" value="{{json_encode($quiz->correct_answer)}}">--}}
+{{--                                        @if($answers == $quiz->correct_answer)--}}
+{{--                                        <label class="form-check-label">--}}
+{{--                                            <input type="text" name="correct_answer" class="form-control" id="exampleInputEmail1" style="color: red; font-weight: bolder" value="{{$answers}}">--}}
+{{--                                        </label>--}}
+{{--                                        @else--}}
+{{--                                            <label class="form-check-label">--}}
+{{--                                                <input type="text" class="form-control" id="exampleInputEmail1" value="{{$answers}}">--}}
+{{--                                            </label>--}}
+{{--                                        @endif--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                        @endforeach--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--        <button type="submit" class="btn btn-block btn-outline-primary col-3">Save</button>--}}
+{{--        <button type="button" class="btn btn-block btn-outline-danger col-3">Cancel</button>--}}
+{{--    </div>--}}
+{{--</form>--}}
+{{--<script src="/plugins/jquery/jquery.min.js"></script>--}}
+{{--<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>--}}
+{{--<script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>--}}
+{{--<script src="/dist/js/adminlte.min.js?v=3.2.0"></script>--}}
+{{--</html>--}}
+
+    <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,7 +97,7 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
-    <link rel= "stylesheet" href="/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="/dist/css/adminlte.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <style>
@@ -27,19 +114,23 @@
             white-space: normal;
             text-overflow: clip;
         }
+        .correct-answer {
+            color: green; /* Màu xanh cho câu trả lời đúng */
+            font-weight: bold;
+        }
     </style>
 </head>
-<form action="{{route('saveAll')}}" method="post">
+<form action="{{ route('saveAll') }}" method="post">
     @csrf
     <div class="container-fluid">
         <div class="row">
-            <input name="topic" value="{{$topic}}">
-            <input name="game" value="{{$game}}">
-            @foreach($decoded->quiz as $quiz)
+            <input name="topic" value="{{ $topic }}" type="hidden">
+            <input name="game" value="{{ $game }}" type="hidden">
+            @foreach($decoded['quiz'] as $index => $quiz)
                 <div class="col-12">
                     <div class="card collapsed-card">
                         <div class="card-header">
-                            <h3 class="card-title">Title</h3>
+                            <h3 class="card-title">{{ $quiz['question'] }}</h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                                     <i class="fas fa-plus"></i>
@@ -49,28 +140,24 @@
                                 </button>
                             </div>
                         </div>
-                        <div class="card-body" style="display: none;">
+                        <div class="card-body">
                             Nội dung câu hỏi
-                            <input name="questions" type="text" class="form-control" id="exampleInputEmail1" value="{{$quiz->question}}">
-                        </div>
-                            @foreach($quiz->answers as $answers)
-                                <div class="card-footer" style="display: none;">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="radio1">
-                                        <input type="hidden" name="answer_content" value="{{json_encode($answers)}}">
-                                        <input type="hidden" name="correct_answer" value="{{json_encode($quiz->correct_answer)}}">
-                                        @if($answers == $quiz->correct_answer)
+                            <input name="questions[]" type="text" class="form-control" value="{{ $quiz['question'] }}" >
+                            @foreach($quiz['answers'] as $key => $answer)
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="answers[{{ $index }}]" value="{{ $key }}">
+                                    @if($answer == $quiz['correct_answer'])
+                                    <label class="form-check-label">
+                                        <input type="text" style="color: green" class="form-control" value="{{ $answer }}">
+                                    </label>
+                                    @else
                                         <label class="form-check-label">
-                                            <input type="text" name="correct_answer" class="form-control" id="exampleInputEmail1"style="color: red; font-weight: bolder" value="{{$answers}}">
+                                            <input type="text" class="form-control" value="{{ $answer }}">
                                         </label>
-                                        @else
-                                            <label class="form-check-label">
-                                                <input type="text" class="form-control" id="exampleInputEmail1" value="{{$answers}}">
-                                            </label>
-                                        @endif
-                                    </div>
+                                    @endif
                                 </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             @endforeach
@@ -79,11 +166,8 @@
         <button type="button" class="btn btn-block btn-outline-danger col-3">Cancel</button>
     </div>
 </form>
-
-
 <script src="/plugins/jquery/jquery.min.js"></script>
 <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <script src="/dist/js/adminlte.min.js?v=3.2.0"></script>
 </html>
-
