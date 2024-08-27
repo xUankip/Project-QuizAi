2f2f073e903c855da21750dd638931c701ec04e8
tuawahn18 <Tuaasnanh188@gmail.com>
2024-08-27 17:34:22 +0700

    sua-form-search-hoan-chinh

--- resources/views/games/searchTopic.blade.php
+++ resources/views/games/searchTopic.blade.php
@@ -12,39 +12,37 @@
     @endif
     @if(isset($topic))
         <div class="main-content">
+            <div class="search-box">
+                <form action="{{ route('searchTopic') }}" method="get">
+                    @csrf
+                    <div class="input-group">
+                        <input type="search" name="topicId" placeholder="Search Data...">
+                        <button type="submit"><img src="/images/img9.png" alt=""></button>
+                    </div>
+                </form>
+            </div>
             <div class="main-content-games">
-                <div class="search-box">
-                    <form action="{{ route('searchTopic') }}" method="get">
-                        @csrf
-                        <div class="input-group">
-                            <input type="search" name="topicId" placeholder="Search Data...">
-                            <button type="submit"><img src="/images/img9.png" alt=""></button>
-                        </div>
-                    </form>
-                </div>
-                <div class="main-content-games">
-                    <form action="{{route('saveId')}}" method="post">
-                        @csrf
-                        <div class="parent">
-                            <input value="topic" name="type" type="hidden">
-                            <input name="type_id" value="{{$topic->id}}" type="hidden">
-                            <div class="card">
-                                <div class="content-box">
-                                    <h1 class="card-topic">{{$topic->name}}</h1>
-                                    <p class="card-description">
-                                        {{$topic->description}}
-                                    </p>
-                                    <button style="background-color: transparent; border : none" type="submit" class="play">
-                                        Show Detail
-                                    </button>
-                                </div>
-                                <div class="qr-box">
-                                    <img src="{{asset('images/img10.jpeg')}}" alt="">
-                                </div>
+                <form action="{{route('saveId')}}" method="post">
+                    @csrf
+                    <div class="parent">
+                        <input value="topic" name="type" type="hidden">
+                        <input name="type_id" value="{{$topic->id}}" type="hidden">
+                        <div class="card">
+                            <div class="content-box">
+                                <h1 class="card-topic">{{$topic->name}}</h1>
+                                <p class="card-description">
+                                    {{$topic->description}}
+                                </p>
+                                <button style="background-color: transparent; border : none" type="submit" class="play">
+                                    Show Detail
+                                </button>
+                            </div>
+                            <div class="qr-box">
+                                <img src="{{asset('images/img10.jpeg')}}" alt="">
                             </div>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     @else
